bb859f7f51a12c97530cffd926445817f4200db6
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-09-26 16:53:37 -0700

    completed firstAndPike

---
+++ app.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var firstAndPike = {
+  minCustomersHour: 23,
+  maxCustomersHour: 65,
+  avgCookiesCustomer: 6.3,
+  randCustomersPerHour: [],
+  randCookiesPerHour: [],
+
+  calcRandCustomersPerHour: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.randCustomersPerHour.push(Math.floor(Math.random() * (this.maxCustomersHour - this.minCustomersHour + 1)) + this.minCustomersHour);
+    }
+  },
+  calcRandCookiesPerHour: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.randCookiesPerHour[i] = Math.round(this.randCustomersPerHour[i] * this.avgCookiesCustomer);
+    }
+  },
+};
+firstAndPike.calcRandCustomersPerHour();
+console.log(firstAndPike.randCustomersPerHour);
+
+firstAndPike.calcRandCookiesPerHour();
+console.log(firstAndPike.randCookiesPerHour);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var sam = {
+//   //properties
+//   firstName: 'Sam',
+//   middleName: null,
+//   lastName: 'Hamm',
+//   rating: 0,
+//   isABoss: true,
+//   underlings: ['Rachel', 'Frazier', 'Aliza', 'Maelle'],
+//
+//   //methods
+//   getRating: function() {
+//     return this.rating;
+//   },
+//   setRating: function(num) {
+//     return this.rating = num;
+//   },
+//   listUnderlings: function(){
+//     for (var i = 0; i < this.underlings.length; i++) {
+//       console.log(this.underlings[i]);
+//     }
+//   }
+//  };
+//
+// sam.employer = {
+//   name: 'Code Fellows',
+//   location: 'Seattle'
+// };
+// //
+// sam.logName = function() {
+//   console.log(this.firstName + ' ' + this.lastName);
+// };
+//
+// sam.whatIsThis = function() {
+//     console.log(this);
+// };
+//
+// sam.whatIsThis(); //logs the sam object
