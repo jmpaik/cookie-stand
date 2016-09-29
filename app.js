e3d4ea919dc487a5c2d3b808429bda4b9d3c5aad
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-09-28 17:09:58 -0700

    reordered my renderings

--- app.js
+++ app.js
@@ -27,35 +27,34 @@ function CookieStand(name, minCustomersHour, maxCustomersHour, avgCookiesCustome
   };
   this.renderTableBody = function () {
     var tableDataDisplay = document.getElementById('salesReport_js');
-    for (var i = 0; i < cookieStands.length; i++) {
-      var trEl = document.createElement('tr'); //Creates rows for business location
-      var tdEl = document.createElement('td'); //Creates columns for business location name
-      tdEl.textContent = cookieStands[i].name; //Adds business location name to columns
-      trEl.appendChild(tdEl); //Adds business location name column to row for current business location[i]
-      for (var a = 0; a < hours.length; a++) {
-        tdEl = document.createElement('td'); //Creates columns for hours
-        tdEl.textContent = cookieStands[i].randCookiesPerHour[a]; //Adds hourly sales of cookies per business location[i] row
-        trEl.appendChild(tdEl); //Adds hourly sales of cookies per business to column for current business location[i]row
-      }
-      tdEl = document.createElement('td');
-      tdEl.textContent = cookieStands[i].CookieSales; //Creates a column for the daily total cookies sold
-      trEl.appendChild(tdEl); //Adds daily cookies total sold to business location row
+    var trEl = document.createElement('tr'); //Creates rows for business location
+    var tdEl = document.createElement('td'); //Creates columns for business location name
+    tdEl.textContent = this.name; //Adds business location name to columns
+    trEl.appendChild(tdEl); //Adds business location name column to row for current business location[i]
+    for (var a = 0; a < hours.length; a++) {
+      tdEl = document.createElement('td'); //Creates columns for hours
+      tdEl.textContent = cookieStands[i].randCookiesPerHour[a]; //Adds hourly sales of cookies per business location[i] row
+      trEl.appendChild(tdEl); //Adds hourly sales of cookies per business to column for current business location[i]row
     }
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.CookieSales; //Creates a column for the daily total cookies sold
+    trEl.appendChild(tdEl); //Adds daily cookies total sold to business location row
     tableDataDisplay.appendChild(trEl); //Adds row for each business location to reporting table
   };
+
   this.calculate = function () {
     this.calcRandCustomersPerHour();
     this.calcRandCookiesPerHour();
-    this.renderTableBody();
   };
   cookieStands.push(this);
   this.calculate();
 }
+
 function renderTableHeader() {
   var tableDataDisplay = document.getElementById('salesReport_js');
   var trEl = document.createElement('tr'); //Creates a row for each business
   var thEl = document.createElement('th'); //Creates a table header column to hold hours
-  thEl.textContent = ''; //Created blank space as the first column in the table header row
+  thEl.textContent = 'Stand Location'; //Created Location cell in the first column in the table header row
   trEl.appendChild(thEl); //Add hours of day to table header row
   for (var i = 0; i < hours.length; i++) {
     thEl = document.createElement('th');//Creates table header row to hold hours of the day
@@ -67,11 +66,12 @@ function renderTableHeader() {
   trEl.appendChild(thEl); //Add totals to table header row in last column
   tableDataDisplay.appendChild(trEl); //Add table header row to daily data table
 };
+
 function renderTableFooter() {
   var tableDataDisplay = document.getElementById('salesReport_js');
   var trEl = document.createElement('tr'); //Create row for business location
   var tdEl = document.createElement('td'); //Create column for business location name
-  tdEl.textContent = 'Totals';
+  tdEl.textContent = 'Hourly Totals';
   trEl.appendChild(tdEl);
   for (var a = 0; a <= hours.length; a++) { //Creates column for each hour of the day
     tdEl = document.createElement('td'); //Creates column for each hour of day
@@ -82,6 +82,7 @@ function renderTableFooter() {
   trEl.appendChild(tdEl); //Adds daily cookies total sold to business location row
   tableDataDisplay.appendChild(trEl); //Adds row for each business location to reporting table
 };
+
 function calcGrandTotal() {
   for (var i = 0; i < hours.length; i++) {
     var hourlyCookieSales = 0;
@@ -94,6 +95,7 @@ function calcGrandTotal() {
     grandTotalSales += cookieStands[y].CookieSales;
   }
 }
+
 renderTableHeader();
 
 new CookieStand('First And Pike', 23, 65, 6.3);
@@ -101,6 +103,10 @@ new CookieStand('Seatac Airport', 3, 24, 1.2);
 new CookieStand('Seattle Center', 11, 38, 3.7);
 new CookieStand('Capitol Hill', 20, 38, 2.3);
 new CookieStand('Alki', 2, 16, 4.6);
-
 calcGrandTotal();
+//this is where you want to do you render method for each object. Maybe a for loop in your array of locations?
+for (var i = 0; i < cookieStands.length; i++) {
+  cookieStands[i].renderTableBody();
+}
+
 renderTableFooter();
